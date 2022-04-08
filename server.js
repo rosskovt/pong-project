516b77b627507fcb0bd9e9204a2eb4eeccb0d91e
rosskovt <rostikk@gmail.com>
2022-04-08 18:31:25 +0300

    finished pong game - multiplayer on localhost

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+const api = require('./api');
 const http = require('http');
-const server = http.createServer();
-
-const io = require('socket.io')(server);
-
+const server = http.createServer(api);
 const PORT = 3000;
+const sockets = require('./sockets');
+
+//const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
+sockets.listen(io);
